958eb5d5a794300ded2545d0303c1a157dd6325d
ersa rani <161242905+ersa-rani@users.noreply.github.com>
2024-11-28 00:49:09 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 
-export default function Home() {
+
+export default function CoinMarket() {
+  const API_KEY = "";
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    fetch(`http://api.coinlayer.com/live?access_key=${API_KEY}`)
+      .then((response) => response.json())
+      .then((jsonConverted) => {
+        setData(jsonConverted);
+      });
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+   
+    <>
+     <div>
+     <div className="bg-cover bg-center h-screen" style={{ backgroundImage: "url('/bg-pic.jpeg')"}}>
+      <div className="main">
+      <h1>CRYPTO CURRENCIES</h1>
+        
+        BTC: {data?.rates?.BTC}
+        <br/>
+        BNB: {data?.rates?.BNB}
+      <br/>
+        ETH: {data?.rates?.ETH}
+        <br/>
+        LTC: {data?.rates?.LTC}
+        <br/>
+        XRP: {data?.rates?.XRP}
+        <br/>
+        DOGE: {data?.rates?.DOGE}
+        <br/>
+        USDT: {data?.rates?.USDT}
+        <br/>
+        BCH: {data?.rates?.BCH}
+        <br/>
+        XLM: {data?.rates?.XLM}
+        <br/>
+        ADA: {data?.rates?.ADA}
+        <br/>
+        SOL: {data?.rates?.SOL}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <section id="footer">
+        <a href="https://www.instagram.com/ersa_rani?igsh=bHoydmptOXMyOGM0" target="_blank" >
+        <FaSquareInstagram className="insta"/></a>
+
+        <a href="https://www.linkedin.com/in/ersa-rani-b4b4842b6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
+        <IoLogoLinkedin className="linkedin" /></a>
+       
+        <a href="https://github.com/ersa-rani" target="_blank">
+        <FaGithub className="github"/> </a>
+       
+      </section>
+        </div>
+        </div>
+     
+    </>
   );
 }
+
+
+
